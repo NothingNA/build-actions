897607c6bab853e6a6b0bb99fff801ede1f2cf7f
NothingNA <105407073+NothingNA@users.noreply.github.com>
2025-12-23 18:36:38 +0800

    Update diy-part.sh

--- build/Official/diy-part.sh
+++ build/Official/diy-part.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展二合一了，在此处可以增加插件
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
-rm -rf package/luci-theme-argon
-git clone https://github.com/NothingNA/luci-theme-argon-person feeds/luci/themes/luci-theme-argon
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
